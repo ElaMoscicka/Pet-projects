c18950d477fb097c7c2d3fab004721afdf51dc79
ElaMoscicka <elakorzeniowska@interia.pl>
2017-12-07 09:28:18 +0100

    Update script.js

--- Bubble Sort/script.js
+++ Bubble Sort/script.js
@@ -1,87 +1,61 @@
 //Bubble Sort:
-var validInputRegExp = /^-?\d+(,-?\d+)*$/;
 
 //Get the number from textarea id numbers:
-function getInput(){
+function getInput() {
   return document.getElementById('numbers').value;
-//"1, 3, 4, 5"
-  /*function getInput(){
-  var input = document.getElementById('numbers').value;
-
-  return input*/
 }
 
-function validateInput(input) {
-    return validInputRegExp.test(input); 
-  /*function validateInput(input) {
-    var isValid = validInputRegExp.test(input);
-  
-  return isValid;
-*/
-}
 
 //Split string value into an array of numbers, checking if current value is a number, removing NaN values:
-function getNumbers(input){
-  return input.split(",").map(number => +number).filter(number => isNaN(number) === false);
-//[1, 3, 4, 5]
-  /*  function getNumbers(input){
-var numbers = input.split(",").map(number => +number).filter(number => isNaN(number) === false);
-  
-  return numbers;*/
+function processInput(input) {
+  var result = {
+    numbers: [],
+    wrongInputs: [],
+    empties: 0//empties is integer  variable 0
+  };
+  var tokens = input.split(',');
+  for (var i = 0; i < tokens.length; i++) {
+    if (!!tokens[i]) { // checks against empty strings
+      var x = Number(tokens[i]); // parses string to number
+      if (!Number.isNaN(x)) { //if x is number ...
+        result.numbers.push(x); // add x to results
+      }
+    }
+  }
+  return result;
 }
 
-  
+
 //Bubble Sort algorithm:
-function bubbleSort(numbers){
- do{
-  var swapped = false;
-  for (var i = 0; i<numbers.length; i++){
-    if (numbers[i]> numbers[i+1]){
-      var temp = numbers[i];
-      numbers[i] = numbers[i+1];
-      numbers[i+1] = temp;
-      swapped = true;
+function bubbleSort(numbers) {
+  do {
+    var swapped = false;
+    for (var i = 0; i < numbers.length; i++) {
+      if (numbers[i] > numbers[i + 1]) {
+        var temp = numbers[i];
+        numbers[i] = numbers[i + 1];
+        numbers[i + 1] = temp;
+        swapped = true;
+      }
     }
   }
- }
-  while(swapped === true)
+  while (swapped === true)
 
-    return numbers;
-  /*function bubbleSort(numbers){
- var length = numbers.length-1;
- do{
-  var swapped = false;
-  for (var i = 0; i<length; i++){
-    if (numbers[i]> numbers[i+1]){
-      var temp = numbers[i];
-      numbers[i] = numbers[i+1];
-      numbers[i+1] = temp;
-      swapped = true;
-    }
-  }
- }
-  while(swapped === true)
-
-    return numbers;*/
+  return numbers;
 }
+
 
 //Get the result from textarea id result:
-function displayResult(result){
+function displayResult(result) {
   document.getElementById('result').value = result;
 }
 
 
 //sorting numbers:
-function runSort(){
+function runSort() {
   var input = getInput();
-  
-  var isValidInput = validateInput(input);
-  
-  if(isValidInput) {
-      var numbers = getNumbers(input);
-      var sortedNumbers = bubbleSort(numbers);
-      displayResult(sortedNumbers);
-  } else {
-    alert('Invalid input!');
-  }
+  var processedInput = processInput(input);
+  var sortedNumbers = bubbleSort(processedInput.numbers);
+  displayResult(sortedNumbers);
 }
+
