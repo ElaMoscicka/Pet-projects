b6c660628ed9bb29d8416a2a143f659df56a1d88
ElaMoscicka <elakorzeniowska@interia.pl>
2018-10-24 22:57:51 +0200

    Add files via upload

---
+++ Quazzu/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+// Custom JS & jQuery here
+$('.carousel.carousel-slider').carousel({fullWidth: true});
+$('.button-collapse').sideNav();
+$('.modal').modal();
+$('select').material_select();
+});
