3688bf048bca87766a04b3289e698af1ae90888d
ElaMoscicka <elmoscic@microsoft.com>
2019-05-01 17:10:51 +0200

    Create SearchBar.js

---
+++ React projects/Pictures/src/components/SearchBar.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+class SearchBar extends React.Component {
+  state = {
+    term: ""
+  };
+
+  onFormSubmit(event) {
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <div className="ui segment">
+        <form onSubmit={this.onFormSubmit} className=" ui form">
+          <div className="field">
+            <label>Image Search</label>
+            <input
+              type="text"
+              value={this.state.term} //storing our information inside of our components,on our state property
+              onChange={e => this.setState({ term: e.target.value })}
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
