67c936726755838df46fddf2f1e4a94a25c9ce00
ElaMoscicka <elmoscic@microsoft.com>
2019-05-30 21:06:22 +0200

    Update App.js

--- React projects/Pictures/src/components/App.js
+++ React projects/Pictures/src/components/App.js
@@ -1,19 +1,18 @@
 import React from "react";
-import unsplash from '../api/unsplash';
+import unsplash from "../api/unsplash";
 import SearchBar from "./SearchBar";
-import ImageList from './ImageList';
+import ImageList from "./ImageList";
 
 class App extends React.Component {
   state = { images: [] };
 
-  onSearchSubmit = async (term) => {
+  onSearchSubmit = async term => {
     const response = await unsplash.get("/search/photos", {
-      params: { query: term },
-      
+      params: { query: term }
     });
 
     this.setState({ images: response.data.results });
-  }
+  };
 
   render() {
     return (
@@ -27,4 +26,3 @@ class App extends React.Component {
 }
 
 export default App;
-
