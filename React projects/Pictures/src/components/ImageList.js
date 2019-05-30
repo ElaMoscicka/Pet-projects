3ddba55457658b40a105fa2d0b06e3c85b3706f2
ElaMoscicka <elmoscic@microsoft.com>
2019-05-30 21:07:22 +0200

    Update ImageList.js

--- React projects/Pictures/src/components/ImageList.js
+++ React projects/Pictures/src/components/ImageList.js
@@ -1,9 +1,10 @@
-import './ImageList.css';
+import "./ImageList.css";
 import React from "react";
+import ImageCard from "./ImageCard";
 
 const ImageList = props => {
-  const images = props.images.map(({ description, id, urls }) => {
-    return <img alt={description} key={id} src={urls.regular} />;
+  const images = props.images.map(image => {
+    return <ImageCard key={image.id} image={image} />;
   });
   return <div className="image-list">{images}</div>;
 };
